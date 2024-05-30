35c20b25a669732dc10a1db8b84495585d973a6b
shivaay2210 <164501782+shivaay2210@users.noreply.github.com>
2024-05-31 02:54:56 +0530

    Updated loginEffects.js

--- loginEffects.js
+++ loginEffects.js
@@ -1,14 +1,53 @@
 const logregBox = document.querySelector('.logreg-box');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
+const forgotLink = document.querySelector('.remember-forgot');
+const forgotForm = document.querySelector('.forgot-password');
 
-registerLink.addEventListener('click', ()=>{
+registerLink.addEventListener('click', () => {
     logregBox.classList.add('active');
 });
 
-loginLink.addEventListener('click', ()=>{
+loginLink.addEventListener('click', () => {
     logregBox.classList.remove('active');
 });
+
+forgotLink.addEventListener('click', () => {
+    logregBox.classList.remove('active');
+
+    document.querySelector('.logreg-box').style.display = 'none';
+
+    if (forgotForm.style.display === 'block') {
+        forgotForm.style.display = 'none'; // Hide the form
+    } else {
+        forgotForm.style.display = 'block'; // Show the form
+    }
+
+});
+
+// const logregBox = document.querySelector('.logreg-box');
+// const loginLink = document.querySelector('.login-link');
+// const registerLink = document.querySelector('.register-link');
+// const forgotLink = document.querySelector('.remember-forgot');
+// const forgotForm = document.querySelector('.forgot-password form')
+
+// registerLink.addEventListener('click', ()=>{
+//     logregBox.classList.add('active');
+// });
+
+// loginLink.addEventListener('click', ()=>{
+//     logregBox.classList.remove('active');
+// });
+
+// forgotLink.addEventListener('click', ()=>{
+//     forgotForm.classList.remove('active');
+//     // alert('hi');
+// });
+
+// forgotLink.addEventListener('click', ()=>{
+//     forgotForm.classList.add('active');
+// });
+
 
 // const buttons = document.querySelectorAll('btn');
 
